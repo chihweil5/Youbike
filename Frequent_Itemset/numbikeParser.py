60ea7bf624fbd1c3c6d140250348bae447d28305
ChihWei <cwl.chihweil@gmail.com>
2017-12-03 17:31:21 -0700

    Filter dataset by empty bike number

--- Frequent_Itemset/numbikeParser.py
+++ Frequent_Itemset/numbikeParser.py
@@ -6,14 +6,17 @@ if __name__ == "__main__":
     
     bike = pd.read_csv("./bike_output.txt")
     
-    fileout = open('frequentItemSet.txt','w')
+    fileout = open('frequentItemSet_time.txt','w')
     fileout.write("ID,Name,Time,Quantity\n")
 
-    bikeFilter = pd.DataFrame(bike[bike['in_use'] <= 5]['week'])
+    #bikeFilter = pd.DataFrame(bike[bike['in_use'] <= 5]['week'])
     
-    for index, item in bike[bike['in_use'] <= 5].iterrows():
-
-        fileout.write(str(item['id'])+','+item['name']+','+'week_'+str(item['week'])+',1\n')
+    for index, item in bike[bike['in_use'] == 0].iterrows():
+    	# get data in a month
+        # month = item['date'].split("-")[1]
+        # if(month == '05'):
+    	# 	fileout.write(str(item['id'])+','+item['name']+','+item['timestamp'].split(":")[0]+',1\n')
+        fileout.write(str(item['id'])+','+item['name']+','+'week_'+str(item['week']+1)+'-'+item['timestamp'].split(":")[0]+',1\n')
         fileout.write(str(item['id'])+','+item['name']+','+item['timestamp'].split(":")[0]+',1\n')
     
 
